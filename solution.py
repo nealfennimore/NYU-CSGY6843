5948c8ca33521f88f1defdd2515ba7659922d15c
Neal Fennimore <hi@neal.codes>
2022-01-26 20:12:34 -0500

    Update format

--- solution.py
+++ solution.py
@@ -5,40 +5,40 @@ import sys
 
 
 def webServer(port=13331):
-  serverSocket = socket(AF_INET, SOCK_STREAM)
-  #Prepare a server socket
-  serverSocket.bind(("127.0.0.1", port))
-  serverSocket.listen()
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Prepare a server socket
+    serverSocket.bind(("127.0.0.1", port))
+    serverSocket.listen()
 
-  while True:
     try:
-      print('Ready to serve...')
-      connectionSocket, addr = serverSocket.accept()
-      try:
-        message = connectionSocket.recv(1024)
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()
+        while True:
+            try:
+                print('Ready to serve...')
+                connectionSocket, addr = serverSocket.accept()
+                try:
+                    message = connectionSocket.recv(1024)
+                    filename = message.split()[1]
+                    f = open(filename[1:])
+                    outputdata = f.read()
 
-        #Send one HTTP header line into socket.
-        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+                    # Send one HTTP header line into socket.
+                    connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
 
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-          connectionSocket.send(outputdata[i].encode())
+                    # Send the content of the requested file to the client
+                    for i in range(0, len(outputdata)):
+                        connectionSocket.send(outputdata[i].encode())
 
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-      except IOError:
-        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-
-    except (ConnectionResetError, BrokenPipeError):
-      pass
+                    connectionSocket.send("\r\n".encode())
+                    connectionSocket.close()
+                except IOError:
+                    connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+                    connectionSocket.close()
+            except (ConnectionResetError, BrokenPipeError):
+                pass
     finally:
-      serverSocket.close()
-      sys.exit()  # Terminate the program after sending the corresponding data
+        serverSocket.close()
+        sys.exit()  # Terminate the program after sending the corresponding data
+
 
 if __name__ == "__main__":
-  webServer(13331)
+    webServer(13331)
